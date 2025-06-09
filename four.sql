2046e35d65682fc54ec489450cea6cdc77e487b3
anshgedia04 <anshgedia03@gmail.com>
2025-06-09 21:03:08 +0530

    GROUP BY

---
+++ four.sql
@@ -0,0 +1,13 @@
+USE TECH_COM;
+
+SELECT * FROM learners
+
+SELECT  COUNT(*) FROM learners
+
+--group by 
+
+SELECT SelectedCourses , COUNT(*)   FROM learners GROUP BY SelectedCourses
+
+--give source of joining with maximum experience
+
+SELECT SourceOfJoining , MAX(YearsOfExperience) as max_exp FROM learners GROUP BY SourceOfJoining
