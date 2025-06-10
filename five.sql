53c26b2fae0ae650248af89f8b996eaba30404f3
anshgedia04 <anshgedia03@gmail.com>
2025-06-10 17:43:29 +0530

    que

--- five.sql
+++ five.sql
@@ -7,3 +7,10 @@ SELECT * FROM courses
 --select those cource which not including "web"
 
 select * from courses where CourseName NOT LIKE "%Web%"
+
+
+SELECT * FROM learners
+
+--give those students whose YearsOfExperience >=3 and SourceOfJoining ="youtube "
+
+SELECT * FROM learners where YearsOfExperience >= 3 AND SourceOfJoining = "YouTube"
