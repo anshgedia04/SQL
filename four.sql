7e66b1eb4e774c8f31226764e6c0eada5dac093f
anshgedia04 <anshgedia03@gmail.com>
2025-06-10 17:30:32 +0530

    --give count of students having soj linked in

--- four.sql
+++ four.sql
@@ -15,3 +15,11 @@ SELECT SourceOfJoining , MAX(YearsOfExperience) as max_exp FROM learners GROUP B
 --give source of joining with avg. experience
 
 SELECT SourceOfJoining , AVG(YearsOfExperience) as avg_exp FROM learners GROUP BY SourceOfJoining
+
+
+SELECT SourceOfJoining , COUNT(*) as avg_exp FROM learners GROUP BY SourceOfJoining HAVING avg_exp > 1 ; 
+
+
+--give count of students having soj linked in
+
+SELECT SourceOfJoining , count(*) as joined_via_linkedIN FROM learners GROUP BY SourceOfJoining HAVING SourceOfJoining = "Seminar"
