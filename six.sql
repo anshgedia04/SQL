e751421a51d38d2a7fa9ca296350ef1d4be654d1
anshgedia04 <anshgedia03@gmail.com>
2025-06-13 21:32:18 +0530

    six

--- six.sql
+++ six.sql
@@ -1,1 +1,9 @@
 USE TECH_COM ;
+
+SELECT * FROM emp;
+--i want number of total emps and avg salary according to location
+
+SELECT Locations , COUNT(*) , avg(salary) FROM emp GROUP BY Locations
+
+
+--now concept of joins comes in
