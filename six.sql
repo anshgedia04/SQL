62b50d398624c58076e389e0610770f7637fae69
anshgedia04 <anshgedia03@gmail.com>
2025-06-14 09:42:17 +0530

    six

--- six.sql
+++ six.sql
@@ -7,3 +7,13 @@ SELECT Locations , COUNT(*) , avg(salary) FROM emp GROUP BY Locations
 
 
 --now concept of joins comes in
+
+--what i want is it includes also firsr name , last name also 
+-- i want fname , lname , location , total emps , avg salary 
+
+
+SELECT emp.fname , emp.lname , emp.Locations , total_emps , avg_salary
+FROM emp
+INNER JOIN (SELECT Locations , COUNT(*) as total_emps , avg(salary) as avg_salary FROM emp GROUP BY Locations) as temp
+ON emp.Locations = temp.Locations
+
