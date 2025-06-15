d77e5021a474b1592d63c3901df75fc7702848d3
anshgedia04 <anshgedia03@gmail.com>
2025-06-15 10:39:40 +0530

    que

--- six.sql
+++ six.sql
@@ -28,4 +28,45 @@ FROM emp
 --window functions 
 --row_rumber 
 
-SELECT fname , lname , salary , ROW_NUMBER() OVER (ORDER BY salary DESC) as Priority_num FROM emp; 
+SELECT fname , lname , salary , ROW_NUMBER() OVER (ORDER BY salary DESC) as Priority_num FROM emp;
+
+--in above case diffn priorities has been given to the same salary
+--that is why rank comes in picture
+
+SELECT fname , lname , salary , RANK() OVER (ORDER BY salary DESC) as Priority_num FROM emp;
+
+
+
+--dense_rank
+
+SELECT fname , lname , salary , DENSE_RANK() OVER (ORDER BY salary DESC) as Priority_num FROM emp;
+
+
+--give me name of the person with  second highest salary 
+
+SELECT * FROM 
+(SELECT fname , lname , salary , DENSE_RANK() OVER (ORDER BY salary DESC) as Priority_num FROM emp) as temp
+where Priority_num = 2
+
+
+--give me details of highest earned people based on location 
+
+--step  one
+SELECT * , ROW_NUMBER() OVER (PARTITION BY Locations ORDER BY salary DESC) from emp
+
+--step 2 
+
+SELECT * FROM 
+(SELECT * , ROW_NUMBER() OVER (PARTITION BY Locations ORDER BY salary DESC) as sal_num from emp) as temp
+where sal_num = 1 ;
+
+
+--another way to achive
+
+--step  one 
+SELECT Locations , max(salary) as max_sal FROM emp GROUP BY Locations
+
+--2
+SELECT fname , lname , salary , emp.Locations FROM emp 
+JOIN (SELECT Locations , max(salary) as max_salary FROM emp GROUP BY Locations) as sub_query
+ON emp.salary = sub_query.max_salary ; 
